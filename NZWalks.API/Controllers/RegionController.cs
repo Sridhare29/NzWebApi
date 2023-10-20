dc869b5e7d66e9c27fe3a5f63d3dc014ba90aa12
Sridhar <sridharonstar@gmail.com>
2023-10-20 12:40:08 +0530

    initial commit

---
+++ NZWalks.API/Controllers/RegionController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class RegionController : ControllerBase
+    {
+        private readonly NZWalksDbContext _dbContext;
+
+        public RegionController(NZWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = _dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var regions = _dbContext.Regions.Find(id);
+            if(regions == null)
+            {
+                return NotFound();
+            }
+            return Ok(regions);
+        }
+
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
+
---
+++ NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace NZWalks.API.Models.Domain
+{
+	public class Region
+	{
+		public Guid Id { get; set; }
+
+        public String Code { get; set; }
+
+        public String Name { get; set; }
+
+        public String? RegionImageUrl { get; set; }
+    }
+}
+
