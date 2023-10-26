338817ab580df1721c740494323b074f2a5ffccb
Sridhar <sridharonstar@gmail.com>
2023-10-26 11:52:07 +0530

    Mvc Get Region Controller update

---
+++ NZWalks.MVC/Controllers/RegionController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NZWalks.MVC.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NZWalks.MVC.Controllers
+{
+    public class RegionController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:7080/api");
+        private readonly HttpClient _client;
+        public RegionController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Region> regionsList = new List<Region>();
+            HttpResponseMessage httpResponse = _client.GetAsync(_client.BaseAddress+ "/Region/GetAll").Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                string data = httpResponse.Content.ReadAsStringAsync().Result;
+                regionsList = JsonConvert.DeserializeObject<List<Region>>(data);
+            }
+            return View(regionsList);
+        }
+    }
+}
+
---
+++ NZWalks.MVC/Models/Region.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace NZWalks.MVC.Models
+{
+	public class Region
+	{
+        public Guid Id { get; set; }
+
+        public String Code { get; set; }
+
+        public String Name { get; set; }
+
+        public String? RegionImageUrl { get; set; }
+    }
+}
+
