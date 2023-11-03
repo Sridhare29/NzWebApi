1ea42d8d893e78897c0d82b4b3b004bb4d71a156
Sridhar <sridharonstar@gmail.com>
2023-11-03 12:02:22 +0530

    Difficulty and region sedding data

--- NZWalks.API/Data/NZWalksDbContext.cs
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -6,7 +6,7 @@ namespace NZWalks.API.Data
 {
 	public class NZWalksDbContext : DbContext
 	{
-		public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+		public NZWalksDbContext(DbContextOptions<NZWalksDbContext> dbContextOptions) : base(dbContextOptions)
 		{
 
 		}
@@ -47,42 +47,42 @@ namespace NZWalks.API.Data
         {
             new Region
             {
-                Id = Guid.Parse("ad1a9601-1d15-4248-aaf4-838ec193465e"),
+                Id = Guid.Parse("69307f13-cd58-4623-88cb-b26a54098fa9"),
                 Code = "AKL",
                 Name = "Auckland",
                 RegionImageUrl="https://img.veenaworld.com/wp-content/uploads/2023/04/Top-10-Best-Hotels-in-the-Magnificent-City-of-Auckland.jpg"
             },
             new Region
             {
-                Id = Guid.Parse("4ad912a1-3d03-495b-a0d9-1f1834b621fe"),
+                Id = Guid.Parse("a9997679-45b9-45ce-a50f-6dabc4564965"),
                 Code = "NTL",
                 Name = "Northland",
                 RegionImageUrl=null
             },
             new Region
             {
-                Id = Guid.Parse("0f4e7d9d-cdd8-4b82-ac68-a33391ae1046"),
+                Id = Guid.Parse("a473c74e-b0a3-46e3-85de-f83b9449991b"),
                 Code = "Bay of Plenty",
                 Name = "BOP",
                 RegionImageUrl="https://www.newzealand.com/assets/Tourism-NZ/Bay-of-Plenty/mountsummit-Katie-Cox__aWxvdmVrZWxseQo_CropResizeWzEyMDAsNjMwLDc1LCJqcGciXQ.jpg"
             },
             new Region
             {
-                Id = Guid.Parse("46828463-1ae4-49bc-ad48-1ef331abdbf9"),
+                Id = Guid.Parse("c9c7c613-f12e-43fd-9f1a-5993cc8f68f0"),
                 Code = "WGN",
                 Name = "Wellington",
                 RegionImageUrl="https://www.planetware.com/wpimages/2022/11/new-zealand-wellington-top-attractions-intro-paragraph-wellington-downtown-harbour.jpg"
             },
             new Region
             {
-                Id = Guid.Parse("a1a8e84e-c785-4079-8e4f-629fbf8ad57b"),
+                Id = Guid.Parse("57ceb9cc-d451-460d-b2f9-c156dbe9bd83"),
                 Code = "NSN",
                 Name = "Nelson",
                 RegionImageUrl="https://images.rawpixel.com/image_450/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAwMi1pc3dhbnRvYS0zNC5qcGc.jpg"
             },
             new Region
             {
-                Id = Guid.Parse(""),
+                Id = Guid.Parse("d446aedc-b6d9-41d3-ae30-0a2e1973c210"),
                 Code = "STL",
                 Name = "Southland",
                 RegionImageUrl=null
---
+++ NZWalks.API/Migrations/20231103063035_seeding data for Difficulities and regions.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedingdataforDifficulitiesandregions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Walks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LengthInKm",
+                table: "Walks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Walks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Regions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Regions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Difficulties",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Difficulties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("1f8b9b6c-1787-4734-b262-5f711cde1d1b"), "Easy" },
+                    { new Guid("524b1946-edfa-4a06-af13-b477ab0a537e"), "Hard" },
+                    { new Guid("711857ad-1245-43f5-880b-5b139f5e461c"), "Medium" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageUrl" },
+                values: new object[,]
+                {
+                    { new Guid("57ceb9cc-d451-460d-b2f9-c156dbe9bd83"), "NSN", "Nelson", "https://images.rawpixel.com/image_450/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAwMi1pc3dhbnRvYS0zNC5qcGc.jpg" },
+                    { new Guid("69307f13-cd58-4623-88cb-b26a54098fa9"), "AKL", "Auckland", "https://img.veenaworld.com/wp-content/uploads/2023/04/Top-10-Best-Hotels-in-the-Magnificent-City-of-Auckland.jpg" },
+                    { new Guid("a473c74e-b0a3-46e3-85de-f83b9449991b"), "Bay of Plenty", "BOP", "https://www.newzealand.com/assets/Tourism-NZ/Bay-of-Plenty/mountsummit-Katie-Cox__aWxvdmVrZWxseQo_CropResizeWzEyMDAsNjMwLDc1LCJqcGciXQ.jpg" },
+                    { new Guid("a9997679-45b9-45ce-a50f-6dabc4564965"), "NTL", "Northland", null },
+                    { new Guid("c9c7c613-f12e-43fd-9f1a-5993cc8f68f0"), "WGN", "Wellington", "https://www.planetware.com/wpimages/2022/11/new-zealand-wellington-top-attractions-intro-paragraph-wellington-downtown-harbour.jpg" },
+                    { new Guid("d446aedc-b6d9-41d3-ae30-0a2e1973c210"), "STL", "Southland", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("1f8b9b6c-1787-4734-b262-5f711cde1d1b"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("524b1946-edfa-4a06-af13-b477ab0a537e"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("711857ad-1245-43f5-880b-5b139f5e461c"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("57ceb9cc-d451-460d-b2f9-c156dbe9bd83"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("69307f13-cd58-4623-88cb-b26a54098fa9"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("a473c74e-b0a3-46e3-85de-f83b9449991b"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("a9997679-45b9-45ce-a50f-6dabc4564965"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("c9c7c613-f12e-43fd-9f1a-5993cc8f68f0"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("d446aedc-b6d9-41d3-ae30-0a2e1973c210"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Walks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LengthInKm",
+                table: "Walks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Walks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Regions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Regions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Difficulties",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
